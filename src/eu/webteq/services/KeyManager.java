7903aac8137d9af8d1c24e6d70827cc10e6ff362
Simon Buckle <simon@webteq.eu>
2011-09-30 12:19:04 +0100

    Applying license terms.

--- src/eu/webteq/services/KeyManager.java
+++ src/eu/webteq/services/KeyManager.java
@@ -1,9 +1,25 @@
+/**
+ * Copyright (C) 2011 Simon Buckle, WebTeq Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.webteq.services;
 
 /**
  * Service responsible for checking and associating users with keys.
  * 
- * @author simon@webteq.eu
+ * @author Simon Buckle <simon@webteq.eu>
  *
  */
 public interface KeyManager {
