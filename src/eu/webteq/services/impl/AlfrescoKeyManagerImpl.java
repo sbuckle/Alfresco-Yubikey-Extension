40431f15813111455a1536b621aee7fa80f61c37
Simon Buckle <simon@webteq.eu>
2011-10-10 16:18:19 +0100

    Checks to see if the username device map has been modified since it was last checked and if so, reloads it.

--- src/eu/webteq/services/impl/AlfrescoKeyManagerImpl.java
+++ src/eu/webteq/services/impl/AlfrescoKeyManagerImpl.java
@@ -31,7 +31,9 @@ public class AlfrescoKeyManagerImpl implements KeyManager {
 	private static final Log logger = LogFactory.getLog(AlfrescoKeyManagerImpl.class);
 	
 	private Map<String, String> usernameKeyMap;
-	private String path; // Path to database file in Alfresco
+	private String path; // Path to database file in Alfresco relative to Company Home
+	private Date lastModified;
+	private NodeRef dbh;
 	
 	// Services
 	protected FileFolderService fileFolderService;
@@ -40,7 +42,12 @@ public class AlfrescoKeyManagerImpl implements KeyManager {
 	protected SearchService searchService;
 	
 	public AlfrescoKeyManagerImpl() {
-		usernameKeyMap = null;
+		usernameKeyMap = new HashMap<String, String>();
+		dbh = null;
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1970, 1, 1);
+		lastModified = calendar.getTime();
 	}
 	
 	public void setFileFolderService(FileFolderService folderService) {
@@ -82,24 +89,45 @@ public class AlfrescoKeyManagerImpl implements KeyManager {
 		return companyHome;
 	}
 	
-	private synchronized String getDeviceId(String username) {
-		if (usernameKeyMap == null) {
-			usernameKeyMap = new HashMap<String, String>();
+	/**
+	 * Resolves the device map to a node reference.
+	 * Caches the reference to avoid looking it up each time.
+	 * 
+	 * @return NodeRef
+	 */
+	private NodeRef getRef() {
+		if (dbh == null) {
 			try {
 				List<String> pathElements = Arrays.asList(getPath().split("/"));
 				FileInfo fi = fileFolderService.resolveNamePath(getCompanyHome(), pathElements);
-				NodeRef db = fi.getNodeRef();
+				dbh = fi.getNodeRef();
+			} catch (FileNotFoundException fnfe) {
+				logger.warn("Failed to initialise user/device map.");
+				throw new AlfrescoRuntimeException("Could not open device map");
+			}
+		}
+		return dbh;
+	}
+	
+	private synchronized String getDeviceId(String username) {
+		NodeRef db = getRef();
+		Date modified = (java.util.Date)nodeService.getProperty(db, ContentModel.PROP_MODIFIED);
+		if (lastModified.before(modified)) {
+			try {
 				ContentReader reader = contentService.getReader(db, ContentModel.PROP_CONTENT);
 				String content = reader.getContentString();
 				String[] entries = content.split("\r?\n");
+				Map<String, String> newEntries = new HashMap<String, String>();
 				for (int i = 0, len = entries.length; i < len; i++) {
 					String entry = entries[i];
 					if (entry.startsWith("#")) { continue; }
 					String[] keyValue = entry.split(":");
-					usernameKeyMap.put(keyValue[0], keyValue[1]);
+					newEntries.put(keyValue[0], keyValue[1]);
 				}
-			} catch (FileNotFoundException fnfe) {
-				logger.warn("Failed to initialise user/device map.");
+				lastModified = modified;
+				usernameKeyMap = newEntries;
+			} catch (Exception ex) {
+				logger.warn("Failed to read database contents.");
 			}
 		}
 		return usernameKeyMap.get(username);
